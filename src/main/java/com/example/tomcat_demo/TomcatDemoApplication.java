8864816d342ed9b2219c89c5e60f170f65c2d30c
manjugoutham <gouthamy219@gmail.com>
2024-08-27 10:17:34 +0530

    Test

---
+++ src/main/java/com/example/tomcat_demo/TomcatDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.tomcat_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomcatDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomcatDemoApplication.class, args);
+	}
+
+}
